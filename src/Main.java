951bbbc97863d0b20d15e18c92cddd42e5b8a6b3
ELano <karadeniz8705@gmail.com>
2023-06-29 01:15:34 +0300

    arrays

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-       double a = 123.4234243;
-       byte b = 127;
+       byte[] arr = {1,6,9,3,4};
+
+        byte a = arr[0];
+        arr[0] = arr[arr.length - 1];
+        arr[arr.length - 1] = a;
+
+        int middleIndex = (int) (Math.floor((float) arr.length / 2));
+        System.out.println(arr[0] + arr[middleIndex]);
     }
 }
