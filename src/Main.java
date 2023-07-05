57adbdac14e3f7f0240f92f282ef3bc883f41bed
ELano <karadeniz8705@gmail.com>
2023-07-06 00:54:38 +0300

    operators-1

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,20 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-       byte[] arr = {1,6,9,3,4};
+       Scanner in = new Scanner(System.in);
 
-        byte a = arr[0];
-        arr[0] = arr[arr.length - 1];
-        arr[arr.length - 1] = a;
+       float x = in.nextFloat();
+       float y = in.nextFloat();
+       float z = in.nextFloat();
 
-        int middleIndex = (int) (Math.floor((float) arr.length / 2));
-        System.out.println(arr[0] + arr[middleIndex]);
+       double m = (x + y + z) / 3;
+       System.out.println(m);
+
+       m = Math.floor(m / 2);
+
+       if(m > 3){
+           System.out.println("Программа выполнена корректно");
+       }
     }
 }
