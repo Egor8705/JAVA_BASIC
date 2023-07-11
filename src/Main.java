6cb485d279ea782ef607621757172fcdbcdf1f4c
ELano <karadeniz8705@gmail.com>
2023-07-12 02:29:56 +0300

    cycles-1

--- src/Main.java
+++ src/Main.java
@@ -2,36 +2,31 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int x = 1;
-        int y = 5;
-        int z = 9;
-        int[] comp = {x,y,z};
-        boolean showMessage = false;
-
         Scanner in = new Scanner(System.in);
         System.out.print("Введите размер массива: ");
 
         int length = in.nextInt();
+        double[] arr = new double[length];
 
         System.out.print("Введите данные массива: ");
         int i = 0;
         while (i < length){
-            int elem = in.nextInt();
-
-            if(!showMessage){
-                for(int n: comp){
-                    if(n == elem){
-                        showMessage = true;
-                        break;
-                    }
-                }
-            }
+            double elem = in.nextDouble();
+            arr[i] = elem;
 
             i++;
         }
 
-        if(showMessage){
-            System.out.println("Данное значение имеется в константах");
+        double sum = 0;
+
+        for(double el: arr){
+            sum += el;
+        }
+
+        double average = sum / length;
+
+        for(double el: arr){
+            System.out.printf("%f ", el * average);
         }
     }
 }
