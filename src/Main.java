ffaafc0113df5feb0b2913123bfbf6bb5cf0f351
ELano <karadeniz8705@gmail.com>
2023-07-12 01:58:43 +0300

    cycles-1

--- src/Main.java
+++ src/Main.java
@@ -2,36 +2,20 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int x = 1;
-        int y = 5;
-        int z = 9;
-        int[] comp = {x,y,z};
-        boolean showMessage = false;
-
         Scanner in = new Scanner(System.in);
-        System.out.print("Введите размер массива: ");
+        System.out.print("Введите число: ");
 
-        int length = in.nextInt();
+        int num = in.nextInt();
 
-        System.out.print("Введите данные массива: ");
-        int i = 0;
-        while (i < length){
-            int elem = in.nextInt();
+        int i = 1;
+        int res = 0;
 
-            if(!showMessage){
-                for(int n: comp){
-                    if(n == elem){
-                        showMessage = true;
-                        break;
-                    }
-                }
-            }
+        while (i <= num){
+            res += i;
 
-            i++;
+            i += 2;
         }
 
-        if(showMessage){
-            System.out.println("Данное значение имеется в константах");
-        }
+        System.out.printf("Сумма всех нечетных чисел от 1 до %d: %d%n",num,res);
     }
 }
