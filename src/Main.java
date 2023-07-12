a182f4eaae0c5241430f5a8bf8fc7025145c0385
ELano <karadeniz8705@gmail.com>
2023-07-13 00:42:22 +0300

    data-1

--- src/Main.java
+++ src/Main.java
@@ -4,27 +4,13 @@ public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
-        System.out.println("Введите размер матрицы");
+        System.out.print("Введите первое число: ");
+        String str = Integer.toString(in.nextInt());
 
-        System.out.print("Введите количество строк: ");
-        int n = in.nextInt();
+        System.out.print("Введите второе число: ");
+        int num = in.nextInt();
 
-        System.out.print("Введите количество столбцов: ");
-        int m = in.nextInt();
-
-        int[][] arr = new int[n][m];
-
-        System.out.print("Введите данные массива: ");
-
-        for (int i = 0;i <n;i++){
-            for (int j = 0;j < m;j++){
-                int elem = in.nextInt();
-                arr[i][j] = elem;
-            }
-        }
-
-        for (int j = 0;j < m;j++){
-            System.out.print(" " + arr[0][j] * 3 + " ");
-        }
+        System.out.println(Math.max(Integer.parseInt(str), num));
+        System.out.println(Double.valueOf(Math.min(Integer.parseInt(str), num)));
     }
 }
