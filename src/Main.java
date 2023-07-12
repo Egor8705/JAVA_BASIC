bbc468e93ab80562a2ac842e723f0cb6cdf08ed7
ELano <karadeniz8705@gmail.com>
2023-07-13 00:04:23 +0300

    cycles-1

--- src/Main.java
+++ src/Main.java
@@ -3,30 +3,28 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.print("Введите размер массива: ");
 
-        int length = in.nextInt();
-        double[] arr = new double[length];
+        System.out.println("Введите размер матрицы");
+
+        System.out.print("Введите количество строк: ");
+        int n = in.nextInt();
+
+        System.out.print("Введите количество столбцов: ");
+        int m = in.nextInt();
+
+        int[][] arr = new int[n][m];
 
         System.out.print("Введите данные массива: ");
-        int i = 0;
-        while (i < length){
-            double elem = in.nextDouble();
-            arr[i] = elem;
 
-            i++;
+        for (int i = 0;i <n;i++){
+            for (int j = 0;j < m;j++){
+                int elem = in.nextInt();
+                arr[i][j] = elem;
+            }
         }
 
-        double sum = 0;
-
-        for(double el: arr){
-            sum += el;
-        }
-
-        double average = sum / length;
-
-        for(double el: arr){
-            System.out.printf("%f ", el * average);
+        for (int j = 0;j < m;j++){
+            System.out.print(" " + arr[0][j] * 3 + " ");
         }
     }
 }
