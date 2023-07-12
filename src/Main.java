56ed28daa3701e890925cfc9f773e6fd45c03745
ELano <karadeniz8705@gmail.com>
2023-07-13 01:00:23 +0300

    data-1

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,18 @@ public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
-        System.out.print("Введите первое число: ");
-        String str = Integer.toString(in.nextInt());
+        System.out.println("Введите: I like Java!!!");
+        String str = in.nextLine()  ;
 
-        System.out.print("Введите второе число: ");
-        int num = in.nextInt();
+        boolean cont = str.contains("Java");
+        boolean start = str.startsWith("I like");
+        boolean end = str.endsWith("!!!");
 
-        System.out.println(Math.max(Integer.parseInt(str), num));
-        System.out.println(Double.valueOf(Math.min(Integer.parseInt(str), num)));
+        if(cont && start && end){
+            System.out.println(str.toUpperCase());
+
+            str = str.replaceAll("a", "o");
+            System.out.println(str.substring(str.indexOf("J"),str.indexOf("J") + 4));
+        }
     }
 }
