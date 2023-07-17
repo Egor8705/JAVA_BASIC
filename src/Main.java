42aee2e84252001415bd86d74ba6b20e494721b2
ELano <karadeniz8705@gmail.com>
2023-07-18 00:10:23 +0300

    final-1

--- src/Main.java
+++ src/Main.java
@@ -1,42 +1,64 @@
 import java.util.Scanner;
 
 public class Main {
-    public static int getUniqueSymbolsCount(String str){
-        String s = str;
-
-        int i = 0;
-        while (i < s.length()){
-            char elem = s.charAt(i);
-            s = elem + s.replaceAll(String.valueOf(elem), "");
-            i++;
-        }
-
-        return s.length();
-    }
-
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
-        System.out.print("Введите количество строк: ");
-        int length = in.nextInt();
+        int tries = 2;
+        int firstTry = 0;
+        int secondTry = 1;
 
-        String[] strArr = new String[length];
+        String answer = "Заархивированный вирус";
+        String bingo = "Правильно!";
+        String help = "Подсказка";
+        String helpStr = "Какая то подсказка";
+        String thinkMore = "Подумай еще!";
+        boolean helpUsed = false;
+        String noHelp = "Подсказка уже недоступна";
+        String fail = "Обидно, приходи в другой раз";
 
-        for(int i = 0;i < strArr.length;i++){
-            System.out.print("Строка " + (i + 1) + ": ");
-            strArr[i] = in.next();
-        }
+        System.out.println("Сидит дед, во сто шуб одет, кто его раздевает, тот слезы проливает");
 
-        int maxUniqueValue = 0;
-        String s = "";
-        for (String str: strArr){
-            int len = getUniqueSymbolsCount(str);
-            if(len > maxUniqueValue){
-                maxUniqueValue = len;
-                s = str;
+        for(int i = firstTry;i <= tries;i++){
+            String attempt = in.nextLine();
+
+            if(answer.equals(attempt)){
+                System.out.println(bingo);
+                return;
             }
+
+             if(i == firstTry){
+                if(help.equals(attempt)){
+                    helpUsed = true;
+                    System.out.println(helpStr);
+                    continue;
+                }
+                    System.out.println(thinkMore);
+                    continue;
+            }
+
+             // По условию не понятно, если ввели "Подсказка" на 2 или 3 попытке,
+             // то попытка является использованной или нет, я допустил что нет
+            if(help.equals(attempt)){
+                System.out.println(noHelp);
+                attempt = in.nextLine();
+            }
+
+            if(answer.equals(attempt)){
+                System.out.println(bingo);
+                return;
+            }
+
+             if(helpUsed){
+                 System.out.println(fail);
+                 return;
+             }
+
+             if(i == secondTry){
+                 System.out.println(thinkMore);
+             }
         }
 
-        System.out.printf("Ответ: %s", s);
+        System.out.println(fail);
     }
 }
