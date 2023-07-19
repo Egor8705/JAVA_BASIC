f34bab8d40e4fe0458660c4f615463c5d8fa5d2d
ELano <karadeniz8705@gmail.com>
2023-07-19 23:19:08 +0300

    arrays-2

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,13 @@ public class Main {
        byte[] arr = {1,6,9,3,4};
 
         byte a = arr[0];
-        arr[0] = arr[arr.length - 1];
-        arr[arr.length - 1] = a;
+        int lastIndex = arr.length - 1;
 
-        int middleIndex = (int) (Math.floor((float) arr.length / 2));
+
+        arr[0] = arr[lastIndex];
+        arr[lastIndex] = a;
+
+        int middleIndex = lastIndex / 2;
         System.out.println(arr[0] + arr[middleIndex]);
     }
 }
